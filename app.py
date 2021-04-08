777718933eaab0278b37d21ce99bea92de89075a
cromewar <vasiliy@cromewar.com>
2021-04-08 16:21:27 -0500

    adding update to crud

--- app.py
+++ app.py
@@ -40,8 +40,26 @@ def get_client(id):
     cur = mysql.connection.cursor()
     cur.execute('select * from clientes where id = %s' % id)
     data = cur.fetchall()
-    print(data)
-    return 'recibido'
+    return render_template('editar_cliente.html', cliente = data[0])
+
+@app.route('/update/<id>', methods = ['POST'])
+def update_client(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        
+    cur = mysql.connection.cursor()
+    cur.execute("""
+                UPDATE clientes
+                set fullname = %s, 
+                phone = %s,
+                email = %s
+                where id = %s
+                """, (fullname, phone, email, id))
+    mysql.connection.commit()
+    flash('Edición de cliente exitosa')
+    return redirect(url_for('Index'))
     
 
 @app.route('/delete/<string:id>')
