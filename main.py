61e1c243efa3f29426471703fbd729ecfcb57872
cromewar <vasiliy@cromewar.com>
2021-04-15 11:08:56 -0500

    adding the log in functionality

---
+++ main.py
@@ -0,0 +1,171 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'mysecretkey'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask_app'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+#Routes
+@app.route('/flaskapp/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect(url_for('home'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
+@app.route('/flaskapp/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+#User registration
+@app.route('/flaskapp/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+#Home Page() route
+# Returns the home page only if the user exist, also takes the "clientes" table
+# and shows the crud for its completion.
+@app.route('/flaskapp/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM clientes')
+        data = cur.fetchall()
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'], clientes= data)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+#Profile
+@app.route('/flaskapp/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+#Routes Related To Crud Functions
+@app.route('/flaskapp/add_client', methods=['POST'])
+def add_client():
+    if request.method == 'POST':
+       fullname = request.form['fullname']
+       phone = request.form['phone']
+       email = request.form['email']
+       cur =  mysql.connection.cursor()
+       cur.execute('INSERT INTO clientes (fullname, phone, email) VALUES (%s, %s, %s)', (fullname, phone, email))
+       mysql.connection.commit()
+       flash('Cliente agregado de manera satisfactoria')
+       return redirect(url_for('home'))
+
+@app.route('/flaskapp/edit/<id>')
+def get_client(id):
+    cur = mysql.connection.cursor()
+    cur.execute('select * from clientes where id = %s' % id)
+    data = cur.fetchall()
+    return render_template('edit_client.html', cliente = data[0])
+
+@app.route('/flaskapp/update/<id>', methods = ['POST'])
+def update_client(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        
+    cur = mysql.connection.cursor()
+    cur.execute("""
+                UPDATE clientes
+                set fullname = %s, 
+                phone = %s,
+                email = %s
+                where id = %s
+                """, (fullname, phone, email, id))
+    mysql.connection.commit()
+    flash('Edición de cliente exitosa')
+    return redirect(url_for('home'))
+    
+
+@app.route('/flaskapp/delete/<string:id>')
+def delete_client(id):
+   cur = mysql.connection.cursor()
+   cur.execute('delete from clientes where id = {0}'.format(id))
+   mysql.connection.commit()
+   flash('Cliente removido de manera exitosa')
+   return redirect(url_for('home'))
+
+#Enable debug mode
+if __name__ == '__main__':
+    app.run(port = 3000, debug=True)
