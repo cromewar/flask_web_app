a9a33a519f22bae7fae6b88883df5f9fdfdb871f
cromewar <vasiliy@cromewar.com>
2021-05-06 12:23:10 -0500

    finishing terapia CRUD

--- blueprints/terapias.py
+++ blueprints/terapias.py
@@ -19,4 +19,63 @@ def home():
     cliente = cur.fetchall()
     cur.execute('SELECT * FROM terapeuta')
     terapeuta = cur.fetchall()
-    return render_template('terapeuta_home.html', terapia=data, terapeuta=data, cliente=cliente)
+    return render_template('terapia_home.html', terapia=data, terapeuta=terapeuta, cliente=cliente)
+
+
+@terapia.route('/add_therapy', methods=['POST'])
+def add_therapy():
+    if request.method == 'POST':
+        fecha = request.form['fecha']
+        costo = request.form['costo']
+        terapeuta = request.form['terapeuta']
+        cliente = request.form['cliente']
+
+        cur = mysql.connection.cursor()
+        cur.execute(
+            'INSERT INTO terapia (fecha, costo, terapeuta_id_terapueta, cliente_id_cliente) VALUES (%s, %s, %s, %s)', (fecha, costo, terapeuta, cliente))
+        mysql.connection.commit()
+        flash('Terapia agregada de manera satisfactoria')
+        return redirect(url_for('terapia.home'))
+
+
+@terapia.route('/edit/<id>')
+def get_terapy(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM terapia WHERE id_terapia  = %s' % id)
+    data = cur.fetchall()
+    cur.execute('SELECT * FROM cliente')
+    cliente = cur.fetchall()
+    cur.execute('SELECT * FROM terapeuta')
+    terapeuta = cur.fetchall()
+    return render_template('edit_terapia.html', terapia=data[0], terapeuta=terapeuta, cliente=cliente)
+
+
+@terapia.route('/update/<id>', methods=['POST'])
+def update_client(id):
+    if request.method == 'POST':
+        fecha = request.form['fecha']
+        costo = request.form['costo']
+        terapeuta = request.form['terapeuta']
+        cliente = request.form['cliente']
+
+    cur = mysql.connection.cursor()
+    cur.execute("""
+                UPDATE terapia
+                set fecha = %s, 
+                costo = %s,
+                terapeuta_id_terapueta = %s,
+                cliente_id_cliente = %s
+                where id_terapia = %s
+                """, (fecha, costo, terapeuta, cliente, id))
+    mysql.connection.commit()
+    flash('Edición de terapia exitosa')
+    return redirect(url_for('terapia.home'))
+
+
+@terapia.route('/delete/<string:id>')
+def delete_therapy(id):
+    cur = mysql.connection.cursor()
+    cur.execute('delete from terapia where id_terapia = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Terapia removida de manera exitosa')
+    return redirect(url_for('terapia.home'))
