c796427ef6ccc6066f22915bddc92d1630917995
cromewar <vasiliy@cromewar.com>
2021-06-03 08:09:56 -0500

    changing the ip for conection

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.register_blueprint(terapia, url_prefix="/terapia")
 app.secret_key = 'mysecretkey'
 
 # Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flask_app'
